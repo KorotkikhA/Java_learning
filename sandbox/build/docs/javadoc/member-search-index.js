a907eac81cf8568c5930de25ad1991a28af532b6
KorotkikhA <antonkorotkikh88@gmail.com>
2021-10-11 22:56:38 +0400

    1

--- sandbox/build/docs/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"MyFirstProgram","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MyFirstProgram","l":"MyFirstProgram()","u":"%3Cinit%3E()"}];updateSearchResults();
