ce628c44395f298cc3e5d20ad03016180462ba2e
KorotkikhA <antonkorotkikh88@gmail.com>
2021-10-12 20:36:14 +0400

    Изменения имени класса

--- sandbox/app/src/test/java/sandbox/AppTest.java
+++ sandbox/app/src/test/java/sandbox/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
+        MyFirstProgram1 classUnderTest = new MyFirstProgram1();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
